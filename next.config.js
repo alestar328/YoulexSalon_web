be940e6c3c05db61cbe86c39b76e6958fe0b254c
alestar328 <alejo1996.96@gmail.com>
2026-03-18 01:18:34 +0100

    github

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+const repo = 'YoulexSalon_web'
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : undefined,
   images: {
     unoptimized: true,
   },
